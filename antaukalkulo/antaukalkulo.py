917d899f183ff4b43dc5b3574565516525e3a62e
Antoni P. Oleksicki <ap.oleksicki@gmail.com>
2020-01-02 13:33:13 +0100

    Add csv based expense store

--- antaukalkulo/antaukalkulo.py
+++ antaukalkulo/antaukalkulo.py
@@ -1,3 +1,4 @@
+import csv
 from dataclasses import dataclass
 from datetime import datetime
 
@@ -7,3 +8,43 @@ class Expense:
     amount: float
     description: str
     created_at: datetime
+
+
+class ExpenseStore:
+    FIELDNAMES = ['Date', 'Description', 'Amount']
+    
+    def __init__(self, storage_file_name: str) -> None:
+        self._storage_file_name = storage_file_name
+        self._initialized = False
+
+    def add(self, expense: Expense) -> None:
+        if not self._initialized:
+            self._initialize()
+
+        with open(self._storage_file_name, 'a+',) as csvfile:
+            writer = csv.DictWriter(
+                csvfile, fieldnames=self.FIELDNAMES, lineterminator='\n',
+            )
+            writer.writerow(
+                {
+                    'Date': expense.created_at,
+                    'Description': expense.description,
+                    'Amount': expense.amount,
+                },
+            )
+
+    def _initialize(self) -> None:
+        with open(self._storage_file_name, 'r') as storage:
+            header_line = storage.readline()
+
+        if header_line != self._build_header_row():
+            with open(self._storage_file_name, 'w') as csvfile:
+                writer = csv.DictWriter(
+                    csvfile, fieldnames=self.FIELDNAMES, lineterminator='\n',
+                )
+                writer.writeheader()
+        self._initialized = True
+
+    @classmethod
+    def _build_header_row(cls) -> str:
+        return ','.join(cls.FIELDNAMES) + '\n'
--- tests/test_antaukalkulo.py
+++ tests/test_antaukalkulo.py
@@ -1,9 +1,47 @@
 from datetime import datetime
+from tempfile import NamedTemporaryFile
 from unittest import TestCase
 
-from antaukalkulo.antaukalkulo import Expense
+from antaukalkulo.antaukalkulo import Expense, ExpenseStore
 
 
-class TestExpense(TestCase):
-    def test_takes_init_parameters(self) -> None:
-        Expense(amount=10, description='foo bar', created_at=datetime.now())
+class TestExpenseStore(TestCase):
+    def setUp(self) -> None:
+        self.storage_file = NamedTemporaryFile()
+        self.expenses = [
+            Expense(
+                amount=10, description='foo bar', created_at=datetime.now(),
+            ),
+            Expense(
+                amount=15, description='baz', created_at=datetime.now(),
+            ),
+        ]
+
+    def test_writes_one_row_to_storage_file(self):
+        store = ExpenseStore(self.storage_file.name)
+        expense = self.expenses[0]
+        store.add(expense)
+        expected_content = (
+            'Date,Description,Amount\n'
+            f'{expense.created_at},{expense.description},{expense.amount}\n'
+        )
+        self.assertEqual(self.storage_file.read().decode(), expected_content,)
+
+    def test_writes_multiple_rows_to_storage_file(self):
+        store = ExpenseStore(self.storage_file.name)
+        for e in self.expenses:
+            store.add(e)
+        self.assertEqual(
+            self.storage_file.read().decode(), self.expected_content(),
+        )
+
+    def expected_content(self) -> str:
+        lines = [
+            'Date,Description,Amount',
+            *((
+                f'{e.created_at},'
+                f'{e.description},'
+                f'{e.amount}'
+            ) for e in self.expenses)
+        ]
+        return '\n'.join(lines) + '\n'
